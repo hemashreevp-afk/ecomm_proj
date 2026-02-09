c870b93b6c73c2747fee8e17c8971324186df472
--Hemashree <--hemashreevp@gmail.com>
2026-02-09 06:00:53 +0530

    Add the app file for configuring the home page for ecomproject and routerlink fornavigation tab for routing fromone page to another page

---
+++ src/app/app.ts
@@ -0,0 +1,16 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header} from './header/header'; // Import the class
+
+@Component({
+  selector: 'app-root',
+  //standalone: true, 
+  imports: [RouterOutlet,Header],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+  
+})
+
+export class App {
+  protected readonly title = signal('ecomm_proj');
+}
