dfa986143f6e47e2a5d4723b43d16fa7e1476d4c
--Hemashree <--hemashreevp@gmail.com>
2026-02-12 06:11:00 +0530

    feat: add routing for seller-home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { SellerAuth } from './seller-auth/seller-auth';
+import { SellerHome } from './seller-home/seller-home'; 
+
 
 
 export const routes: Routes = [
   { path: 'home', component: Home },
   { path: 'seller-auth', component: SellerAuth },
+  {path: 'seller-home', component: SellerHome},
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
