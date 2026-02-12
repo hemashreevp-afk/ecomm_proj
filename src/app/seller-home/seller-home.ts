0660dbe342d2890a24265306e3e4707d8aa4bcec
--Hemashree <--hemashreevp@gmail.com>
2026-02-12 06:21:16 +0530

    feat: add seller-home folder for post-signup navigation

---
+++ src/app/seller-home/seller-home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './seller-home.html',
+  styleUrl: './seller-home.css',
+})
+export class SellerHome {
+
+}
--- src/app/services/seller.ts
+++ src/app/services/seller.ts
@@ -6,10 +6,10 @@ import { HttpClient } from '@angular/common/http';
 })
 export class Seller   {
   constructor(private http: HttpClient) {}
-  userSignUp(data:any){
-    //console.warn("seller sign up")
-    return this.http.post('http://localhost:3000/seller',data)  //posting the data to json server for seller sign up  
-   // console.warn("result");
+  userSignUp(data:object){
+    console.warn("seller sign up")
+    return this.http.get('http://localhost:3000/seller')  //adding the data to json server for seller sign up  
+    console.warn("result");
   }
 
 }
