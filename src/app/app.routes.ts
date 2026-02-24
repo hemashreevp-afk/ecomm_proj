d0188d42c52940e10e08634309e46fef811cd573
--Hemashree <--hemashreevp@gmail.com>
2026-02-24 06:55:10 +0530

    add the routing path  for seller-add-product and auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Home } from './home/home';
 import { SellerAuth } from './seller-auth/seller-auth';
 import { SellerHome } from './seller-home/seller-home'; 
 import { AuthGuard } from './auth-guard';
+import { SellerAddProduct } from './seller-add-product/seller-add-product';
 
 
 
@@ -10,5 +11,7 @@ export const routes: Routes = [
   { path: 'home', component: Home },
   { path: 'seller-auth', component: SellerAuth },
   {path: 'seller-home', component: SellerHome,canActivate: [AuthGuard]},//seller-home component is protected by the authGuard, which means that only authenticated users will be able to access it. You can implement your authentication logic in the authGuard to determine whether the user is authenticated or not.
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
+  { path: 'seller-add-product',component:SellerAddProduct},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  
 ];
