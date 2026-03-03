9a6e644abf30a6b771e1379ffbc906dbf1b46590
--Hemashree <--hemashreevp@gmail.com>
2026-03-03 17:57:28 +0530

    changed the imageurl to imageUrl and add id  field for each interface sign ,login and product

--- src/app/data-type.ts
+++ src/app/data-type.ts
@@ -1,17 +1,20 @@
 export interface signup{
+  id: number;
   name: string;
   email: string;
   password: string;
 }
 export interface login{ 
+  id: number;
   email: string;
   password: string;
 }
 export interface Product{
+  id: number;
   name: string;
   price: number;
   colour: string;
   description: string;
-  imageurl: string;
+  imageUrl: string;
   category: string;
 }
