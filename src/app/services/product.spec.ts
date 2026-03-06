dd745de2b8f952c1b2ef3c86a528087157c98f29
--Hemashree <--hemashreevp@gmail.com>
2026-03-06 06:29:47 +0530

    fix: rename Product service to ProductService to resolve naming conflict with Product interface

--- src/app/services/product.spec.ts
+++ src/app/services/product.spec.ts
@@ -1,13 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { ProductService } from './product';
 
-import { Product } from './product';
-
-describe('Product', () => {
-  let service: Product;
+describe('ProductService', () => {
+  let service: ProductService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(Product);
+    service = TestBed.inject(ProductService);
   });
 
   it('should be created', () => {
