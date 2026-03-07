a1570452c17295476fe23b19020a7ae26174f16b
--Hemashree <--hemashreevp@gmail.com>
2026-03-07 22:42:20 +0530

    change style tage from marig:left5% to margin-left:5% and comments commom-form with typo errror

---
+++ src/app/seller-update-product/seller-update-product.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-update-product',
+  imports: [],
+  templateUrl: './seller-update-product.html',
+  styleUrl: './seller-update-product.css',
+})
+export class SellerUpdateProduct {
+
+}
