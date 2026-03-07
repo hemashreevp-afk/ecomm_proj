63f234c0d4a408bf6f96a87abf626c4665b109e6
--Hemashree <--hemashreevp@gmail.com>
2026-03-07 06:53:07 +0530

    Refactor product image handling: replace imageUrl with image and store uploaded image file path

--- src/app/seller-add-product/seller-add-product.ts
+++ src/app/seller-add-product/seller-add-product.ts
@@ -14,12 +14,34 @@ import { Product } from '../data-type';
 })
 export class SellerAddProduct implements OnInit  {
   addProductMessage: string |undefined; //used to store the success message when a product is added successfully
+   selectedFile: File | null = null;//used to store the selected file for product image upload  
+  imagePreview: string | ArrayBuffer | null = null;//preview of selecteed image before uploading
+  
   constructor(private productService: ProductService) { }
-
+  
   ngOnInit(): void {
+   
+
 
   } 
+
+  onFileSelected(event: any) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.selectedFile = file;
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imagePreview = reader.result;
+      };
+      reader.readAsDataURL(file);   // IMPORTANT
+    }
+  }
+
   submit(data:Product): void {
+     if (this.selectedFile) {
+    data.imageUrl = 'assets/images/' + this.selectedFile.name;
+    console.warn('imageUrl set to:', data.imageUrl);
+  }
     console.warn('Product data submitted:', data);
     this.productService.addProduct(data).subscribe((response) => {
       console.log('Product added successfully:', response);
@@ -27,7 +49,10 @@ export class SellerAddProduct implements OnInit  {
       if(response){
         this.addProductMessage = 'Product added successfully!';
       }
-    });
+    }, (error) => {
+      console.error('Error adding product:', error);
+      alert('Error adding product!');
+    } );
     setTimeout(() => {
       this.addProductMessage = undefined; // Clear the message after 3 seconds
     }, 3000);
