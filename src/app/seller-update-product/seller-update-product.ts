b6a25d17c73e23f2e06eecb2da64a8bf81dd8dc4
--Hemashree <--hemashreevp@gmail.com>
2026-03-11 20:27:43 +0530

    fix: add the fa-icon update  in view and function for delete  product

--- src/app/seller-update-product/seller-update-product.ts
+++ src/app/seller-update-product/seller-update-product.ts
@@ -1,11 +1,96 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {FormsModule, NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product';
+import { Product } from '../data-type';
 
 @Component({
   selector: 'app-seller-update-product',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './seller-update-product.html',
   styleUrl: './seller-update-product.css',
 })
-export class SellerUpdateProduct {
+
+
+export class SellerUpdateProduct implements OnInit {
+  delProductMessage: string |undefined; //used to store the success message when a product is added successfully
+   selectedFile: File | null = null;//used to store the selected file for product image upload  
+  imagePreview: string | ArrayBuffer | null = null;//preview of selecteed image before uploading
+  productData = {id:0 ,name: '',description:'',category:'',colour:'',imageUrl:'', price: 0,  } // initialize all required fields
+//productData: Product | undefined;// declared for update the product data in  update form
+
+  constructor(private route:ActivatedRoute, private product:ProductService){}
+  ngOnInit(): void {
+  let productId=this.route.snapshot.paramMap.get('id'); //getting the product for populate updation
+  console.log(productId);// to show product id
+   
+  productId && this.product.getProduct(productId).subscribe((data)=>{
+    console.log(data);//product data fetched  usingproduct id
+    this.productData=data;
+    // show existing image
+  this.imagePreview = data.imageUrl;
+  })
+    
+  }
+
+  /*
+  // function for  uplaod the file url and preview the file
+  onFileSelected(event: any) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.selectedFile = file;
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imagePreview = reader.result;
+        this.productData.imageUrl = reader.result as string; // update imageUrl for submission
+      };
+       reader.readAsDataURL(file);   // IMPORTANT
+    }
+  }*/
+
+ // IMAGE SELECT
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.selectedFile = file.name;
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imagePreview = reader.result;
+      };
+      reader.readAsDataURL(file);
+      this.productData.imageUrl = 'assets/images/' + file.name; //set new image after selection
+      this.imagePreview=this.productData.imageUrl;// Set imageUrl for submission
+    }
+  }
+
+
+
+
+  // Submit form data
+  submit(formValue: Product){
+    // Prepare FormData for backend
+    const formData = new FormData();
+    formData.append('id', this.productData.id.toString());
+    formData.append('name', this.productData.name);
+    formData.append('description', this.productData.description);
+    formData.append('price', this.productData.price.toString());
+    formData.append('category', this.productData.category);
+    formData.append('colour', this.productData.colour);
+
+
+  if (this.selectedFile) {
+  formData.append('image', this.selectedFile); // send real file
+} else {
+  formData.append('imageUrl', this.productData.imageUrl);
+}
+
+ console.log('Form data to submit:', formValue); // Log the form data for debugging
+ if(this.productData){
+  formValue.id=this.productData.id;
+ console.log('Form data to submit:', this.productData)
+  
+ }
+}
 
 }
--- src/app/services/product.ts
+++ src/app/services/product.ts
@@ -9,15 +9,30 @@ import { SellerHome } from '../seller-home/seller-home';
 export class ProductService {
   
   constructor(private http:HttpClient) { } 
+
+// aapi called for adding  product
   addProduct(data: Product) {
     console.warn('Product data submitted:', data);
-    return this.http.post(`http://localhost:3000/products`, data);// add api  for add product
+    return this.http.post(`http://localhost:3000/products`, data);
   }
+
+// api called  for get product list
   productList(){
-    return this.http.get<Product[]>(`http://localhost:3000/products`);// add api  for get product list
+    return this.http.get<Product[]>(`http://localhost:3000/products`);
   } 
+
+  //api called   for  delete the product using product id
   deleteProduct(id: number)
   {
     return this.http.delete(`http://localhost:3000/products/${id}`);// add api  for delete product
+  }
+
+  //api called for get the product details using product id for updating the product details
+  getProduct(id:string){
+    return this.http.get<Product>(`http://localhost:3000/products/${id}`);
+  }
+
+  updateProduct(product: Product) {
+    return this.http.put(`http://localhost:3000/products/${product.id}`, product);
   }
 }
