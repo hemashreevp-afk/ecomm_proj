85b0e40360b5c10aaf3dd159438d153e9116bf00
--Hemashree <--hemashreevp@gmail.com>
2026-03-15 06:32:11 +0530

    fix:created function popularProduct for display the product in home -ccarousel

--- src/app/services/product.ts
+++ src/app/services/product.ts
@@ -35,4 +35,8 @@ export class ProductService {
   updateProduct(product: Product) {
     return this.http.put(`http://localhost:3000/products/${product.id}`, product);
   }
+
+  popularProducts() {
+    return this.http.get<Product[]>("http://localhost:3000/products?_limit=3");
+  }
 }
