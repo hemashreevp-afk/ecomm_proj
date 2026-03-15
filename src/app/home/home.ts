920d8ef30b9c011e93e52c18972691e23490fdce
--Hemashree <--hemashreevp@gmail.com>
2026-03-15 06:17:01 +0530

    Add ng-bootstrap carousel for popular products on home page, create style for  home page using home-carousel, called function for display top 3 productin home carousel

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product';
+import { Product } from '../data-type';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgbCarouselModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
+export class Home implements OnInit {
+popularProducts:any[]=[];//used to store the list of popular products fetched from the API
 
+  constructor(private product: ProductService) { }
+  ngOnInit(): void {
+    this.product.popularProducts().subscribe((data) => {
+      console.log(data);
+      this.popularProducts = data;
+    })
+  }
 }
+
