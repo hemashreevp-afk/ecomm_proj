d7d32aa7c78d173bd9ffc22f2d4ebae55f8d99ff
--Hemashree <--hemashreevp@gmail.com>
2026-03-21 14:16:53 +0530

    fix: add product search and limit suggestion dropdown to 5 items

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -2,49 +2,72 @@ import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink, NavigationEnd } from '@angular/router';
 import { Route } from '@angular/router';
 import { filter } from 'rxjs/operators';
-import { TitleCasePipe } from '@angular/common';
-import { ProductService } from '../services/product'; 
+import { CommonModule, TitleCasePipe } from '@angular/common';
+import { ProductService } from '../services/product';
+import { Product } from '../data-type';
+import { ChangeDetectorRef } from '@angular/core';
 
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, TitleCasePipe],
+  imports: [RouterLink, TitleCasePipe, CommonModule],
   standalone: true,
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header implements OnInit {
-    sellerName:string=" ";
+  sellerName: string = " ";
+  menuType: string = 'default';//used to determine which menu to display based on the user's login status and current route
+  searchResult: Product[]|null = [];;// tostore the array product search
 
- menuType: string = 'default';//used to determine which menu to display based on the user's login status and current route
-
-  constructor(private route: Router, private productService: ProductService) { }
+    constructor(
+    private route: Router,
+    private productService: ProductService,
+    private cdr: ChangeDetectorRef
+  ) { }
 
   ngOnInit(): void {
-  
-this.route.events.subscribe((event) => {
-  if (event instanceof NavigationEnd) {
-    const currentUrl = event.url;
-    console.log('Current URL:', currentUrl);
-    if (currentUrl) {
-      if (localStorage.getItem('seller') && currentUrl.startsWith('/seller')) {
-        console.log('This seller route and seller is logged in');
-       let sellerStore=localStorage.getItem('seller');
-       let sellerData=sellerStore && JSON.parse(sellerStore);
-       this.sellerName = sellerData.name;//storing the seller name in a variable to display it in the header
-      this.menuType = 'seller';
-      } else {
-        console.log('outside seller route or seller not logged in');
-        this.menuType = 'default';
+
+    this.route.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const currentUrl = event.url;
+        console.log('Current URL:', currentUrl);
+        if (currentUrl) {
+          if (localStorage.getItem('seller') && currentUrl.startsWith('/seller')) {
+            console.log('This seller route and seller is logged in');
+            let sellerStore = localStorage.getItem('seller');   // store the seller details
+            let sellerData = sellerStore && JSON.parse(sellerStore);
+            this.sellerName = sellerData.name;//storing the seller name in a variable to display it in the header
+            this.menuType = 'seller';
+          } else {
+            console.log('outside seller route or seller not logged in');
+            this.menuType = 'default';
+          }
+        }
       }
-    }
-  }
-});
+    });
 
   }
-logout():void{
-localStorage.removeItem('seller');
-this.sellerName="";
-this.route.navigate(['/'])
-}
+
+  //logout function
+  logout(): void {
+    localStorage.removeItem('seller');
+    this.sellerName = "";
+    this.route.navigate(['/'])
+  }
+//function of search product
+  searchProduct(query: KeyboardEvent) {
+    if (query) {//check for key pressed or not 
+      const element = query.target as HTMLInputElement;
+      console.log(element.value);//value pressed from keyboard
+      this.productService.searchProduct(element.value)
+        .subscribe((result) => {
+           this.searchResult = result.slice(0, 5); // ✅ limit to 5 items // detect the change in service and update view
+          console.log(this.searchResult)
+          this.cdr.detectChanges();
+           
+        });
+
+    }
+  }
 }
--- src/app/services/product.ts
+++ src/app/services/product.ts
@@ -44,4 +44,9 @@ export class ProductService {
   {
     return this.http.get<Product[]>("http://localhost:3000/products?_limit=8");
   }
+
+  //auto suggestion based on keyword enterin input text box
+  searchProduct(query: string) {
+  return this.http.get<Product[]>(`http://localhost:3000/products?q=${query}`);
+}
 }
