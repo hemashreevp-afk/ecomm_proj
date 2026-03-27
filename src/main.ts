d6929f0fecf172b2eef9fbbd423367821c4b9a99
--Hemashree <--hemashreevp@gmail.com>
2026-03-27 20:29:14 +0530

    feat: add router configuration using provideRouter

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 import '@angular/localize/init';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+
