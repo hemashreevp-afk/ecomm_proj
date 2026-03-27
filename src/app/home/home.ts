9ab9d22cc0073a8c1762bce67faa703f8e5ad1d8
--Hemashree <--hemashreevp@gmail.com>
2026-03-27 20:41:19 +0530

    chore:add  ChangeDetectorRef from Home component for automatic update  of view page

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ChangeDetectorRef} from '@angular/core';
 import { ProductService } from '../services/product';
 import { Product } from '../data-type';
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
@@ -15,19 +15,23 @@ import { CommonModule } from '@angular/common';
 export class Home implements OnInit {
 popularProducts:any[]=[];//used to store the list of popular products fetched from the API
 trendyProducts:any[]=[];//to show the trendy product list on home page
-  constructor(private product: ProductService) { }
+  constructor(
+    private product: ProductService,
+  private cd: ChangeDetectorRef) { }
   ngOnInit(): void {
 
     //product for display  home cauorsel
     this.product.popularProducts().subscribe((data) => {
       console.log(data);
       this.popularProducts = data;
+       this.cd.detectChanges(); 
     })
 
     //product for display on trendy side
     this.product.trendyProducts().subscribe((data) => {
       console.log(data);
       this.trendyProducts = data;
+       this.cd.detectChanges(); 
     })
 
   }
