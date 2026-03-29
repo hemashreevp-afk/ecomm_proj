e3625cf8ccd3f5b106df307e585333d1aca69a13
--Hemashree <--hemashreevp@gmail.com>
2026-03-29 12:55:29 +0530

    Added SweetAlert popup with HTML, CSS, and TS implementation

--- src/app/alert-commands/alert-commands.ts
+++ src/app/alert-commands/alert-commands.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-alert-commands',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './alert-commands.html',
   styleUrl: './alert-commands.css',
 })
 export class AlertCommands {
+ visible = false;
+  type: 'success' | 'warning' = 'success';
+  title = '';
+  message = '';
 
+  @Output() onConfirm = new EventEmitter<void>();
+
+
+
+  show(type: 'success' | 'warning', title: string, message: string) {
+    this.type = type;
+    this.title = title;
+    this.message = message;
+    this.visible = true;
+  }
+
+  close() {
+    this.visible = false;
+  }
+
+  confirm() {
+    this.onConfirm.emit();
+    this.close();
+  }
 }
